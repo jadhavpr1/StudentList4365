b568526d2fd903b9aa2946502d3155021d5d595c
jadhavpr1 <jadhavpr@sheridancollege.ca>
2020-01-16 11:34:36 -0500

    Updated with the getter and setter

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -8,6 +8,8 @@ package studentlist;
 public class Student {
 
     private String name;
+    
+    private String address;
 
     public Student(String name) {
         this.name = name;
@@ -17,6 +19,14 @@ public class Student {
         return name;
     }
 
+     public String getAddress() {
+        return address;
+    }
+    
+     public void setAddress(String address) {
+        this.name = address;
+    }
+    
     public void setName(String name) {
         this.name = name;
     }
